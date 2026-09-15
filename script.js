9c743924a25d3fcc29ca3769d5265f2e84fbc36c
LWattrus <leightonwattrus@yahoo.com>
2026-09-15 16:26:10 +0900

    Update API endpoint for loading league data

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ const leagueId = 2326400;
 
 async function loadLeague() {
     try {
-        const response = await fetch(
-            `https://fantasy.premierleague.com/api/leagues-classic/${leagueId}/standings/`
-        );
+        const response = await fetch("/.netlify/functions/fpl");
+
+        if (!response.ok) {
+            throw new Error("Could not load FPL data");
+        }
 
         const data = await response.json();
 
-        console.log(data);
+        console.log("FPL data loaded:", data);
 
     } catch (error) {
         console.error("Error loading FPL data:", error);
